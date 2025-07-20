54a98fe6e787df7261c5b5cfb3b1401eeb87b4d1
KRISH141006 <krishsondagar13@gmail.com>
2025-07-21 01:30:39 +0530

    tried new things

--- practise_files/connecting_Wifi.c
+++ practise_files/connecting_Wifi.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
     char userName[50];
     char instruction[500];
+    char ask;
     // scanf("%s",instruction);
     // fgets(instruction,sizeof(instruction),stdin);
     // printf("%s\n",instruction);
     // printf("%d",strcmp("connect wifi=Krish",instruction));
 
+    
+    
     while(1){
+        
+        int check = system("ping www.google.com");
+        printf("checking wifi is connected or not : %d",check);
 
-        printf("give instructions : ");
-        gets(instruction);
-        if(!strcmp("connect wifi=Krish",instruction)){
-            printf("connecting with wifi...\n"); 
-            system("C:\\windows\\system32\\netsh.exe wlan connect name = Krish");  
-        }else if(!strcmp("disconnect wifi",instruction)){
-            printf("disconnecting wifi...\n");
-            system("C:\\windows\\system32\\netsh.exe wlan disconnect");
-        }else{
-            printf("waiting for instructions\n");
+        if(check!=0){
+            printf("You are not connected with wifi do you want to connect (y/n) : ");
+            scanf("%c",ask);
+            ask = tolower(ask);
+            switch(ask){
+                case 'y': system("%systemroot%\\system32\\netsh.exe interface set interface name=\"Wi-Fi\" admin=enable && netsh wlan show network"); break;
+            };
+            break;
         }
+
+        // printf("give instructions : ");
+        // gets(instruction);
+        // if(!strcmp("connect wifi=Krish",instruction)){
+        //     printf("connecting with wifi...\n"); 
+        //     system("%systemroot%\\system32\\netsh.exe wlan connect name = Krish");  
+        // }else if(!strcmp("disconnect wifi",instruction)){
+        //     printf("disconnecting wifi...\n");
+        //     system("%systemroot%\\system32\\netsh.exe wlan disconnect");
+        // }else{
+        //     printf("waiting for instructions\n");
+        // }
         
     }
  
